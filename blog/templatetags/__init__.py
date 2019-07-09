d1db0285ce1260b9ac2622ed190951004e12709b
leader1993 <1470106994@qq.com>
2019-07-09 18:08:54 +0800

    Initial commit

---
+++ blog/templatetags/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     __init__.py  
+   Description :  
+   Author :       JHao
+   date：          2017/4/14
+-------------------------------------------------
+   Change Activity:
+                   2017/4/14: 
+-------------------------------------------------
+"""
+__author__ = 'JHao'
+#python3不需要下面的代码
+'''
+import sys
+import importlib
+importlib.reload(sys)
+sys.setdefaultencoding('utf-8')
+
+if __name__ == '__main__':
+    pass
+'''
